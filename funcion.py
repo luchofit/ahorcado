749f06c42a347295a76e4a666c248704ffb267e2
Luis Castro <luisf.castroc.1995@gmail.com>
2022-01-06 17:40:35 -0400

    agregando función limpiar pantalla

--- funcion.py
+++ funcion.py
@@ -1,3 +1,11 @@
+import os
+def clear(): #También la podemos llamar cls (depende a lo que estemos acostumbrados)
+    if os.name == "posix":
+        os.system ("clear")
+    elif os.name == ("ce", "nt", "dos"):
+        os.system ("cls")
+
+
 def run():
     pass
 
