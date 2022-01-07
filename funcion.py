9eacd943d68e506abd42ccbf6d720ebf29d56e94
Luis Castro <luisf.castroc.1995@gmail.com>
2022-01-06 20:14:43 -0400

    ingreso base de datos

--- funcion.py
+++ funcion.py
@@ -1,13 +1,22 @@
 import os
+import random
+
+
 def clear(): #También la podemos llamar cls (depende a lo que estemos acostumbrados)
-    if os.name == "posix":
-        os.system ("clear")
-    elif os.name == ("ce", "nt", "dos"):
         os.system ("cls")
 
+def read_data():
+    data = []
+    with open("./ahorcados/data.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            data.append(line)
 
 def run():
     pass
 
+
+
 if __name__ == "__main__":
+    clear()
+    read_data() 
     run()
